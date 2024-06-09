29471923e4e77d401dfe3fbdfc5f7d324d44d212
Nada-Marzouk <nadamarzouk05@gmail.com>
2024-06-09 21:55:10 +0200

    updated forecast

--- index.js
+++ index.js
@@ -1,71 +1,48 @@
 // https://api.weatherapi.com/v1/forecast.json?key=09120181217647d3b9754953232711&q=cairo&days=3
 let date = new Date();
-let weekDays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thuresday",
-  "Friday",
-  "Saturday",
-];
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
+let weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thuresday","Friday","Saturday",];
+let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+let hours = ["00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00",]
 let searchInput = document.getElementById("searchInput");
 // today catches
-let today = document.getElementById("today");
-let todayDate = document.getElementById("todayDate");
-let currentCity = Array.from(document.querySelectorAll(".currentCity"));
-let todayTemp = document.getElementById("todayTemp");
-let tempIcon = document.getElementById("tempIcon");
-let todayStatus = document.getElementById("todayStatus");
-let percentage = document.getElementById("percentage");
-let windVelocity = document.getElementById("windVelocity");
-let direction = document.getElementById("direction");
-let todayIcon = document.getElementById("todayIcon");
-let humidity = document.getElementById("humidity");
+let cities = Array.from(document.querySelectorAll(".city"));
 // next days catches
 let nextDay = Array.from(document.querySelectorAll(".nextDay"));
-let nextIcon = Array.from(document.querySelectorAll(".nextIcon"));
-let nextTemp = Array.from(document.querySelectorAll(".nextTemp"));
-let nextStatus = Array.from(document.querySelectorAll(".nextStatus"));
+let days = Array.from(document.querySelectorAll(".days"));
+let dates = Array.from(document.querySelectorAll(".dates"));
+let temps = Array.from(document.querySelectorAll(".temp"));
+let icons = Array.from(document.querySelectorAll(".icon"));
+let statuss = Array.from(document.querySelectorAll(".statue"));
+let forecastHours = Array.from(document.querySelectorAll(".forecastHours"));
+let humidityContainer = Array.from(document.querySelectorAll(".humidity"));
+let windVelocityContainer = Array.from(document.querySelectorAll(".windVelocity"));
+let directionContainer = Array.from(document.querySelectorAll(".direction"));
 
-function displayTodayWhether(city, temp, src, Status, h, windV, dir) {
-  today.innerHTML = weekDays[date.getDay()];
-  todayDate.innerHTML = `${date.getDate()}${months[date.getMonth()]}`;
-  for (let i = 0; i < currentCity.length; i++) {
-    currentCity[i].innerHTML = city;
-  }
-  todayTemp.innerHTML = `${temp}<sup>o</sup>c`;
-  todayIcon.setAttribute("src", `http:${src}`);
-  todayStatus.innerHTML = Status;
-  humidity.innerHTML = `<i class="fas fa-umbrella"></i> ${h}%`;
-  windVelocity.innerHTML = `<i class="fas fa-wind"></i> ${windV}km/h`;
-  direction.innerHTML = `<i class="fas fa-compass"></i> ${dir}`;
-}
-function displayNextDayWhether(holder) {
-  let nextSrc, nTemp;
-  for (let i = 0; i < nextDay.length; i++) {
-    nextDay[i].innerHTML =
-      weekDays[new Date(holder.forecast.forecastday[i + 1].date).getDay()];
-    nextSrc = holder.forecast.forecastday[i + 1].day.condition.icon;
-    nTemp = holder.forecast.forecastday[i + 1].day.avgtemp_c;
-    nextIcon[i].setAttribute("src", `https:${nextSrc}`);
-    nextTemp[i].innerHTML = `${nTemp}<sup>o</sup>c`;
-    nextStatus[i].innerHTML =
-      holder.forecast.forecastday[i + 1].day.condition.text;
+function displayDayWeather(api) {
+  let forecastdays = api.forecast.forecastday
+  for (let i = 0; i < forecastdays.length; i++) {
+    days[i].innerHTML=weekDays[new Date(forecastdays[i].date).getDay()]
+    dates[i].innerHTML=`${new Date(forecastdays[i].date).getDate()}${months[date.getMonth()]}`
+    cities.map(c=>c.innerHTML=api.location.name)
+    temps[i].innerHTML=`${forecastdays[i].day.avgtemp_c}<sup>o</sup>c`
+    icons[i].setAttribute("src",`https:${forecastdays[i].day.condition.icon}`)
+    statuss[i].innerHTML=forecastdays[i].day.condition.text
+    let hourBox=``
+    for (let j = 0; j < forecastdays[i].hour.length; j++) {
+      hourBox+=`
+          <div class="single-hour px-3 py-1 rounded-2 text-center">
+                    <p class="text-white my-0">${hours[j]}</p>
+                    <div class="hour-contain my-0">
+                      <img class="w-100" src="http:${forecastdays[i].hour[j].condition.icon}"/>
+                    </div>
+                    <p class="  my-0 fs-4 text-info">${forecastdays[i].hour[j].condition.text}</p>
+                    <p class="text-white fs-3 mb-0">${forecastdays[i].hour[j].temp_c}</p>
+                  </div>
+      `
+    }
+    forecastHours[i].innerHTML=hourBox
+    humidityContainer[i].innerHTML=`<i class="fas fa-umbrella"></i> ${forecastdays[i].day.avghumidity}%`
+    windVelocityContainer[i].innerHTML=`<i class="fas fa-wind"></i> ${forecastdays[i].day.maxwind_kph}%`
   }
 }
 async function getWhetherApiData(q) {
@@ -73,16 +50,7 @@ async function getWhetherApiData(q) {
     `https://api.weatherapi.com/v1/forecast.json?key=09120181217647d3b9754953232711&q=${q}&days=3`
   );
   let apiData = await apiResponse.json();
-  displayTodayWhether(
-    apiData.location.name,
-    apiData.current.temp_c,
-    apiData.current.condition.icon,
-    apiData.current.condition.text,
-    apiData.current.humidity,
-    apiData.current.wind_kph,
-    apiData.current.wind_dir
-  );
-  displayNextDayWhether(apiData);
+  displayDayWeather(apiData)
 }
 searchInput.addEventListener("keyup", function () {
   getWhetherApiData(searchInput.value);
